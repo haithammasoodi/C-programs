65a799a14d411dcd4161851fa62ce659b9629a64
Haitham Masoodi <95998118+haithammasoodi@users.noreply.github.com>
2022-09-22 07:43:18 +0530

    Update pointers_array.c

--- pointers_array.c
+++ pointers_array.c
@@ -5,8 +5,8 @@ int main()
 	int i,n;
 	printf("Number of elements ");
 	scanf("%d", &n);
-	int arr[n];
-	int *p = &arr[0];
+	int arr[n],*p;
+	p = &arr[0];
 	printf("Enter the elements of the array \n");
 	for (i=0;i<n;i++)
 	{
