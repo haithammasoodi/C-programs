35e3b7ead40eb7beb444b174474567863ca45255
Haitham Masoodi <95998118+haithammasoodi@users.noreply.github.com>
2022-09-22 07:44:26 +0530

    Update pointers_array_reverse.c

--- pointers_array_reverse.c
+++ pointers_array_reverse.c
@@ -5,8 +5,8 @@ int main()
 	int i,n;
 	printf("Number of elements ");
 	scanf("%d", &n);
-	int arr[n];
-	int *p = &arr[n-1];
+	int arr[n],*p;
+	p = &arr[n-1];
 	printf("Enter the elements of the array \n");
 	for (i=0;i<n;i++)
 	{
